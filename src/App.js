6e3d1b7789f01b39d31f7ff6aebc5c8030aa5daa
sanyaodare@gmail.com <sanyaodare@gmail.com>
2020-10-20 10:28:42 +0100

    add Todos Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import Todos from './components/Todos';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>App</h1>
+        <Todos />
       </div>
     );
   }  
