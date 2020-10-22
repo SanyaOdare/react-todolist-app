924cb8ec87a1cc2dd86b936d55a56c5e79bddaed
sanyaodare@gmail.com <sanyaodare@gmail.com>
2020-10-22 06:14:51 +0100

    add toggle state

--- src/App.js
+++ src/App.js
@@ -4,10 +4,40 @@ import Todos from './components/Todos';
 import './App.css';
 
 class App extends Component {
-  render() {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meeting with boss',
+        completed: false
+      },
+    ]
+  }
+
+  // Toggle Complete
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+
+  render() {    
     return (
       <div className="App">
-        <Todos />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     );
   }  
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
+import TodoItem from './Todoitem';
+import PropTypes from 'prop-types';
 
 class Todos extends Component {
   render() {
-    return (
-      <div>
-        <h1>Todos</h1>
-      </div>
-    );
+    return this.props.todos.map((todo) => (
+      <TodoItem key={todo.id} todo={todo} markComplete={ this.props.markComplete} />
+    ));
   }  
+}
+
+// PropTypes
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired
 }
 
 export default Todos;
