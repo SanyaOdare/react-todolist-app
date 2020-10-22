893ed26a5ab302b2d5ef565d122c3aab8e0cf2dc
sanyaodare@gmail.com <sanyaodare@gmail.com>
2020-10-22 06:51:12 +0100

    add delete button

--- src/App.js
+++ src/App.js
@@ -34,10 +34,15 @@ class App extends Component {
     }) });
   }
 
+  // Delete Todo
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id )] });
+  }
+
   render() {    
     return (
       <div className="App">
-        <Todos todos={this.state.todos} markComplete={this.markComplete} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
       </div>
     );
   }  
